ab0059642ee3ffb4e8f118b6ae2e70c2be5562f8
4zazel <michal.sztaba@op.pl>
2022-07-16 19:33:42 +0200

    unity

---
+++ Test/Assets/PlayerControler.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControler : MonoBehaviour
+{
+
+    public float moveSpeed = 1f;
+
+    public float CollisionOffset = 0.05f;
+
+    public ContactFilter2D movementFilter;
+
+    Vector2 movementInput;
+
+    Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if(movementInput != Vector2.zero)
+        {
+            int count = rb.Cast(
+                movementInput,
+                movementFilter,
+                castCollisions,
+                moveSpeed * Time.fixedDeltaTime + CollisionOffset);
+
+            if(count==0){
+                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            }
+        }
+
+
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+    }
+}
